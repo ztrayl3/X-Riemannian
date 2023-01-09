b62d7138f2f663c89a53e3bed1c5011c40f122d6
zatraylo <zatraylo@ncsu.edu>
2023-01-09 12:38:03 +0100

    Attempting submission of jobs in small groups to find the source of performance problems (+ Stats updates)

--- Plafrim/RunMS.sh
+++ Plafrim/RunMS.sh
@@ -4,11 +4,11 @@
 # Note: we ignore subjects A04, A09, A17, A29, A41, B78, and B79 due to being listed as "bad" in the original paper
 cond="MS_RG_Between"
 
-#for i in {1..3} {5..8} {10..16} {18..28} {30..40} {42..60}
-#do
-#  sbatch launch.sh A"$i" $cond
-#done
-# ONLY LAUNCHING B AND C JOBS
+for i in {1..3} {5..8} {10..16} {18..28} {30..40} {42..60}
+do
+  sbatch launch.sh A"$i" $cond
+done
+ ONLY LAUNCHING B AND C JOBS
 
 for i in {61..77}
 do
--- Stats/Analysis.R
+++ Stats/Analysis.R
@@ -2,41 +2,13 @@ library(readr)
 library(ggplot2)
 library(plotly)
 
-source <- read_csv("A18_1671123794633043.csv", col_types = cols(...1 = col_skip()))
+source <- read_csv("A1_16730323238023405.csv", col_types = cols(...1 = col_skip()))
 
-# Look at timeseries importance
-Timewise <- data.frame(time=source$Time, weight=source$Weight)
-Timewise <- aggregate(weight~time, data=Timewise, FUN=sum)
-ggplotly(
-  ggplot(Timewise, aes(x=time, y=weight)) +
-    geom_area(fill="#69b3a2", alpha=0.5) +
-    geom_line(color="#69b3a2") +
-    ylab("Feature Weight (LIME)") +
-    xlab("Time (in EEG samples)") + 
-    geom_smooth(method = "lm", formula = y ~ poly(x, 15), se = FALSE)
-)
+# Simple analysis of classifier accuracy
+ggplot(source, aes(fill=Classifier, y=Accuracy, x=Subject)) + 
+  geom_bar(position="dodge", stat="identity")
 
-# Look at channel importance
-Channelwise <- data.frame(channel=source$Channel, weight=source$Weight)
-Channelwise <- aggregate(weight~channel, data=Channelwise, FUN=sum)
-Channelwise <- Channelwise[order(Channelwise$channel, decreasing = TRUE),]
-
-ggplotly(
-  ggplot(data=Channelwise, aes(x=reorder(channel, -weight), y=weight)) +
-    geom_bar(stat="identity") + 
-    xlab("Channel")
-)
-
-# Look at left and right, overlaid
-Timewise.left <- subset(source, source$Predicted == "left")
-Timewise.left <- aggregate(Timewise.left[, 5], list(Timewise.left$Time), sum)
-Timewise.right <- subset(source, source$Predicted == "right")
-Timewise.right <- aggregate(Timewise.right[, 5], list(Timewise.right$Time), sum)
-
-ggplotly(
-  ggplot(Timewise.left, aes(x=Group.1, y=Weight)) +
-  geom_line(color="#69b3a2") +
-  geom_line(data = Timewise.right, aes(x=Group.1, y=Weight)) +
-  ylab("Feature Weight (LIME)") +
-  xlab("Time (in EEG samples)")
-)
+channels <- aggregate(Weight ~ Channel, data = source, sum)[order(-channels$Weight),]
+channels$Channel <- factor(channels$Channel, levels = channels$Channel)
+ggplot(channels, aes(y=Weight, x=Channel)) + 
+  geom_bar(stat="identity")
