7a5120492bdcf0e4594422b16caccd22d62d6a5e
zatraylo <zatraylo@ncsu.edu>
2023-01-10 15:13:06 +0100

    Runtime boosted to 12 hours, just to be safe. Properly loading the DL modules so that DL conditions can be run, and finalizing the RunMS file.

--- Plafrim/RunMS.sh
+++ Plafrim/RunMS.sh
@@ -3,7 +3,7 @@
 # Run all MS subjects through a given condition
 # Note: we ignore subjects A04, A09, A17, A29, A41, B78, and B79 due to being listed as "bad" in the original paper
 cond="MS_RG_Between"  # chosen condition
-target="zonda"  # chosen HPC node
+target="zonda"  # chosen HPC node, note that DL conditions need GPU nodes!
 N=21  # how many nodes target has
 
 for i in {1..3} {5..8} {10..16} {18..28} {30..40} {42..60}
--- Plafrim/launch.sh
+++ Plafrim/launch.sh
@@ -2,14 +2,13 @@
 #SBATCH --job-name=X_Riemann                                       # Job name
 #SBATCH --ntasks=1                                                 # Run on a single CPU
 #SBATCH --mem=32gb                                                 # Job memory request
-#SBATCH --time=10:00:00                                            # Time limit hrs:min:sec
+#SBATCH --time=12:00:00                                            # Time limit hrs:min:sec
 #SBATCH --output=/beegfs/ztraylor/X-Riemannian/Output/%j.log       # Standard output and error log
 
 # Load modules
 module purge  # unload all modules
 module load language/python/3.9.6  # load python 3.9.6
-module load compiler/cuda/11.6  # load cuda for DL
-module load dnn/cudnn/9.0-v7.1  # load cuDNN
+module load dnn/cudnn/11.2-v8.1.1.33  # load cuda and cudnn for DL
 
 # activate Venv
 cd /beegfs/ztraylor/X-Riemannian
